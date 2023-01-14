fc8dcabbe686a80dd4f815c2ff4e81441a81b7a2
leo.jjcc <1542123803@qq.com>
2023-01-14 14:18:25 +0800

    init

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,16 @@
  * Copyright(c) 2022 长沙市希尚网络科技有限公司
  * 注意：本内容仅限于长沙市希尚网络科技有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
- 
 
 
 /**
  * TODO
- * 
+ *
  * @author ${USER}
  * @version 1.0 ${DATE}
- */public class Main {
+ */
+public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
